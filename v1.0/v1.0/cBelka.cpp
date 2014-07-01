80c97814697d62a494808359646389380a030894
kbloszyk94 <k.bloszyk94@wp.pl>
2014-07-01 13:26:45 +0200

    rysowanie belek

--- v1.0/v1.0/cBelka.cpp
+++ v1.0/v1.0/cBelka.cpp
@@ -16,6 +16,7 @@ cBelka::cBelka(float xp, float yp, float xk, float yk, float wytrzymalosc)
 	kolor_g = 0.3;
 	kolor_b = 0;
 	z = 0;
+	grubosc = 0.5;
 }
 
 cBelka::~cBelka(void)
@@ -28,12 +29,18 @@ void cBelka::Rysuj()
 	glTranslatef(0.5 * (x_poczatku + x_konca), 0.5 * (y_poczatku + y_konca), z);
 	glRotatef(atan2((y_konca - y_poczatku),(x_konca - x_poczatku)), 0, 0, 1);
 	glColor3f(kolor_r, kolor_g, kolor_b);
-	//glBegin(GL_QUADS);
-			//glTexCoord2f(0.0f, 0.0f); glVertex2f(-2.5*rozmiar, -rozmiar);
-			//glTexCoord2f(1.0f, 0.0f); glVertex2f( 2.5*rozmiar, -rozmiar);
-			//glTexCoord2f(1.0f, 1.0f); glVertex2f( 2.5*rozmiar,  rozmiar);
-			//glTexCoord2f(0.0f, 1.0f); glVertex2f(-2.5*rozmiar,  rozmiar);
-	//glEnd();
+	glBegin(GL_POLYGON);
+		glVertex3f(-0.5 * (x_poczatku + x_konca),grubosc / 2, 0);
+		glVertex3f(-0.5 * (x_poczatku + x_konca),grubosc / 2, -grubosc);
+		glVertex3f(-0.5 * (x_poczatku + x_konca),-grubosc / 2, -grubosc);
+		glVertex3f(-0.5 * (x_poczatku + x_konca),-grubosc / 2, 0);
+	glEnd();
+	glBegin(GL_POLYGON);
+		glVertex3f(0.5 * (x_poczatku + x_konca),grubosc / 2, 0);
+		glVertex3f(0.5 * (x_poczatku + x_konca),grubosc / 2, -grubosc);
+		glVertex3f(0.5 * (x_poczatku + x_konca),-grubosc / 2, -grubosc);
+		glVertex3f(0.5 * (x_poczatku + x_konca),-grubosc / 2, 0);
+	glEnd();
 	glDisable(GL_TEXTURE_2D);
 	glPopMatrix();
 }
